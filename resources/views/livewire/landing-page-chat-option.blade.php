7c81447e04d1142a8b1efb40f8e1207525f32a99
Nishant Kedare <kedarenishant11111@gmail.com>
2024-07-06 16:44:57 +0530

    Landing Page Created

---
+++ resources/views/livewire/landing-page-chat-option.blade.php
@@ -0,0 +1,25 @@
+<?php
+
+use function Livewire\Volt\{state};
+
+//
+
+?>
+
+<div class="fixed bottom-4 right-8 w-min flex justify-between gap-4">
+    <div class="w-full flex items-center">
+        <div class="bg-white rounded-2xl capitalize text-lg whitespace-nowrap py-2 px-4 h-min"> Ready to change your life
+        </div>
+    </div>
+    <div class="bg-[#f6aa23] rounded-full p-4">
+        <svg class="w-8 h-8 text-[#050e14]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+            height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd"
+                d="M4 3a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h1v2a1 1 0 0 0 1.707.707L9.414 13H15a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4Z"
+                clip-rule="evenodd" />
+            <path fill-rule="evenodd"
+                d="M8.023 17.215c.033-.03.066-.062.098-.094L10.243 15H15a3 3 0 0 0 3-3V8h2a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1h-1v2a1 1 0 0 1-1.707.707L14.586 18H9a1 1 0 0 1-.977-.785Z"
+                clip-rule="evenodd" />
+        </svg>
+    </div>
+</div>
