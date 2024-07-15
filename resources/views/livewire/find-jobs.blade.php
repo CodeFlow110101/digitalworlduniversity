335c9cfc27da1a23bc8f2fd31baa4faab78ad930
Nishant Kedare <kedarenishant11111@gmail.com>
2024-07-15 21:31:26 +0530

    Add job functionality created

--- resources/views/livewire/find-jobs.blade.php
+++ resources/views/livewire/find-jobs.blade.php
@@ -1,11 +1,43 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{with, on};
+use App\Models\Job;
 
-//
+with(fn() => ['jobs' => Job::paginate(0)]);
+
+on(['reset-find-jobs-page' => function () {
+    $this->reset();
+}]);
+
 
 ?>
 
 <div>
-    <button wire:click="$dispatch('show-modal', { modal:'add-job', args:null, data:null, callback_event:null })" class="bg-blue-500 rounded-lg py-2 px-4">Click</button>
+    <button wire:click="$dispatch('show-modal', { modal:'add-job', args:null, data:null, callback_event:null })" class="fixed z-10 bottom-12 right-12 hover:bg-gray-300 bg-[#d6dcde] text-xl flex justify-between items-center gap-4 rounded-lg py-2 px-4">
+        <div>
+            <svg class="w-6 h-6 text-[#131e30]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                <path fill-rule="evenodd" d="M10 2a3 3 0 0 0-3 3v1H5a3 3 0 0 0-3 3v2.382l1.447.723.005.003.027.013.12.056c.108.05.272.123.486.212.429.177 1.056.416 1.834.655C7.481 13.524 9.63 14 12 14c2.372 0 4.52-.475 6.08-.956.78-.24 1.406-.478 1.835-.655a14.028 14.028 0 0 0 .606-.268l.027-.013.005-.002L22 11.381V9a3 3 0 0 0-3-3h-2V5a3 3 0 0 0-3-3h-4Zm5 4V5a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v1h6Zm6.447 7.894.553-.276V19a3 3 0 0 1-3 3H5a3 3 0 0 1-3-3v-5.382l.553.276.002.002.004.002.013.006.041.02.151.07c.13.06.318.144.557.242.478.198 1.163.46 2.01.72C7.019 15.476 9.37 16 12 16c2.628 0 4.98-.525 6.67-1.044a22.95 22.95 0 0 0 2.01-.72 15.994 15.994 0 0 0 .707-.312l.041-.02.013-.006.004-.002.001-.001-.431-.866.432.865ZM12 10a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z" clip-rule="evenodd" />
+            </svg>
+        </div>
+        <div class="text-[#131e30] font-semibold">
+            Add Job
+        </div>
+    </button>
+
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+        @foreach($jobs as $job)
+        <div class="bg-[#d6dcde] rounded-2xl grid grid-cols-1 gap-4">
+            <div class="flex items-center justify-center w-full h-48 bg-gray-500 rounded-t-2xl">
+                <svg class="w-10 h-10 text-gray-300" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                    <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                </svg>
+            </div>
+            <div class="p-4 text-[#131e30] grid grid-cols-1 gap-4">
+                <div class="font-semibold text-2xl">{{$job->title}}</div>
+                <div class="font-semibold text-md capitalize">{{$job->description}}</div>
+            </div>
+            <a href="{{$job->url}}" target="_blank" class="text-[#d6dcde] bg-[#131e30] rounded-b-2xl text-center py-3 text-lg font-bold">Apply</a>
+        </div>
+        @endforeach
+    </div>
 </div>
