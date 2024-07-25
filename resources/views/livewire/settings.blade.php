f20b9438ade9477ac1b00852483746a448eb06f6
Nishant Kedare <kedarenishant11111@gmail.com>
2024-07-25 20:04:23 +0530

    Profile Photo Functionality Created

--- resources/views/livewire/settings.blade.php
+++ resources/views/livewire/settings.blade.php
@@ -1,12 +1,42 @@
 <?php
 
 use App\Models\LkUserPlan;
+use App\Models\User;
 use App\Models\Wallet;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Storage;
 
-use function Livewire\Volt\{mount, state};
+use function Livewire\Volt\{mount, state, on};
 
-state(['user', 'balance', 'days_remaining', 'expiry_date',]);
+state(['balance', 'days_remaining', 'expiry_date',]);
+
+state(['user'])->reactive();
+
+on(['settings-handle-file' => function ($validationKey, $validationMessage, $thumbnailName, $thumbnailPath) {
+
+    if ($validationKey && $validationKey['thumbnail']) {
+
+        if ($validationKey['thumbnail']) {
+            $this->addError('thumbnail', $validationMessage['thumbnail']);
+        }
+
+        $this->dispatch('admin-panel-modal-store-loader', value: false);
+    } else {
+        if ($this->user->image) {
+            Storage::disk('public')->delete($this->user->image);
+        }
+        User::where('id', $this->user->id)->update(['image' => $thumbnailPath]);
+        $this->dispatch('reset-user-landing-page');
+    }
+}]);
+
+$removePhoto = function () {
+    if ($this->user->image) {
+        Storage::disk('public')->delete($this->user->image);
+    }
+    User::where('id', $this->user->id)->update(['image' => null]);
+    $this->dispatch('reset-user-landing-page');
+};
 
 mount(function ($user) {
     $this->$user = $user;
@@ -41,8 +71,31 @@ mount(function ($user) {
         </button>
     </div>
 
-    <div class="text-[#131e30] grid grid-cols-1 gap-8 sm:gap-20 text-xl lg:text-3xl">
-        <div class="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-20 text-center font-bold text-xl lg:text-2xl">
+    <div class="text-[#131e30] grid grid-cols-1 gap-6 sm:gap-10 text-xl lg:text-3xl">
+        <div class="flex justify-center items-center">
+            <div class="grid grid-cols-1 gap-4">
+                <div class="flex justify-center">
+                    @if($user->image)
+                    <img src="{{asset('storage/'.$user->image)}}" class="w-24 h-24 lg:w-28 lg:h-28 rounded-full">
+                    @else
+                    <svg class="w-24 h-24 lg:w-28 lg:h-28 text-[#131e30]" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                        height="24" fill="currentColor" viewBox="0 0 24 24">
+                        <path fill-rule="evenodd"
+                            d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z"
+                            clip-rule="evenodd" />
+                    </svg>
+                    @endif
+                </div>
+                <div class="flex justify-center">
+                    <div class="flex justify-between gap-3 font-semibold">
+                        <div wire:click="removePhoto" class="bg-[#131e30] px-8 py-4 text-lg font-semibold rounded-lg text-[#d6dcde] cursor-pointer">Remove</div>
+                        <div @click="$refs.photo.click()" class="bg-[#131e30] px-8 py-4 text-lg font-semibold rounded-lg text-[#d6dcde] cursor-pointer">Edit</div>
+                        <input wire:input="$dispatch('upload-thumbnail', { thumbnail: $refs.photo, thumbnailSizeLimit:1, callbackDispatch:'settings-handle-file', callbackLoaderDispatch:'settings-loader'})" x-ref="photo" type="file" accept="image/*" class="block hidden px-2.5 pb-2.5 pt-4 w-full text-sm text-[#131e30] bg-transparent rounded-lg border-2 border-[#131e30] appearance-none focus:outline-none focus:ring-0 peer" placeholder=" " />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="grid grid-cols-1 gap-8 lg:grid-cols-3 lg:gap-16 text-center font-bold text-xl lg:text-2xl">
             <div class="bg-[#d6dcde] rounded-2xl py-8 lg:py-12">Student ID: {{$user->id}}</div>
             <div class="bg-[#d6dcde] rounded-2xl py-8 lg:py-12">{{$days_remaining}} Days</div>
             <div class="bg-[#d6dcde] rounded-2xl py-8 lg:py-12">Wallet: ${{$balance}}</div>
