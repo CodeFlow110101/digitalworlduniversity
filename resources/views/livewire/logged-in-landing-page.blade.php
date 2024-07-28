2218de90b1e80d7eef9996f97557649a9f4688a4
Nishant Kedare <kedarenishant11111@gmail.com>
2024-07-28 18:51:16 +0530

    Fix

--- resources/views/livewire/logged-in-landing-page.blade.php
+++ resources/views/livewire/logged-in-landing-page.blade.php
@@ -88,7 +88,7 @@ mount(function (Request $request) {
         <div class="lg:w-9/12 w-full py-6 px-4 lg:px-8 grid grid-cols-1 gap-8 h-min">
             <div class="flex justify-end">
                 <label class="inline-flex items-center cursor-pointer">
-                    <input wire:click="$toggle('darkmode')" wire:model="darkmode" type="checkbox" value="" class="sr-only peer">
+                    <input wire:click="$toggle('darkmode')" wire:model="darkmode" type="checkbox" value="{{$darkmode}}" class="sr-only peer">
                     <div class="relative w-11 h-6 bg-gray-200  peer-focus:outline-none rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                     <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Dark Mode</span>
                 </label>
