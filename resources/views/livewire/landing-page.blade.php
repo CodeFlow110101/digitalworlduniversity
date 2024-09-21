dbe30bff6fec7b3ca35b33082aa23d4be3b227ae
Nishant Kedare <kedarenishant11111@gmail.com>
2024-09-21 13:49:01 +0530

    Video added on the Landing Page

--- resources/views/livewire/landing-page.blade.php
+++ resources/views/livewire/landing-page.blade.php
@@ -28,15 +28,10 @@ $redirectTo = function ($path) {
             </div>
             <div class="flex justify-center">
                 <div class="border border-gray-300 bg-gray-800 rounded-lg p-2 lg:w-3/5 w-11/12">
-                    <div role="status"
-                        class="flex border border-gray-300 items-center sm:h-80 h-48 justify-center bg-gray-600 rounded-lg">
-                        <svg class="w-10 h-10 text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                            fill="currentColor" viewBox="0 0 16 20">
-                            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
-                            <path
-                                d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z" />
-                        </svg>
-                    </div>
+                    <video class="w-full h-full rounded-lg" controls autoplay muted loop>
+                        <source src="{{asset('videos/IMG_5884.mp4')}}" type="video/mp4">
+                        Your browser does not support the video tag.
+                    </video>
                 </div>
             </div>
             <div class="flex justify-center pt-8">
