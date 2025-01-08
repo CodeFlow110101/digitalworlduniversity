8ae2790e3954614e33812a2e62f9c379cf949a4a
codeflow <codeflow110101@gmail.com>
2025-01-08 19:44:14 +0530

    Resolving Migration

--- database/migrations/2024_12_29_103852_rename_option_column_in_earn_money_options_table.php
+++ database/migrations/2024_12_29_103852_rename_option_column_in_earn_money_options_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('earn_money_options', function (Blueprint $table) {
-            //
+            $table->renameColumn('text', 'option');
         });
     }
 };
